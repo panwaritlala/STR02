ab40a1fb38fcb6443132747c0a2c4ce0fcbfd92c
panwaritlala <pornkornpen@gmail.com>
2023-12-15 14:28:03 +0700

    .

--- home.py
+++ home.py
@@ -5,3 +5,10 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('panwarit suriwan')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/263829-qgdtri-1.n..jpg')
+with col2:
+    st.image('./pic/unnamed.jpg')
