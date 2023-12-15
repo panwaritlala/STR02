65676b5587435ddf0d6ecad811b57a5006eb5354
panwaritlala <pornkornpen@gmail.com>
2023-12-15 14:35:12 +0700

    .

--- home.py
+++ home.py
@@ -14,3 +14,10 @@ with col1:
 
 with col2:
     st.image('./pic/unnamed.jpg', width=200)
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
