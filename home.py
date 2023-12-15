89e9c6b371c6c6a6961c3783ec34837d7c12e474
panwaritlala <pornkornpen@gmail.com>
2023-12-15 14:31:30 +0700

    .

--- home.py
+++ home.py
@@ -1,14 +1,18 @@
 import streamlit as st
 
-st.header("star")
+st.header("Star")
 st.title('การทดสอบเขียนเว็บด้วย Python')
-st.header('panwarit suriwan')
+st.header('Panwarit Suriwan')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
 col1, col2 = st.columns(2)
-#col1.write("This is column 1")
-#col2.write("This is column 2")
+
+# เพิ่มข้อความหรือข้อมูลเพิ่มเติมในแต่ละคอลัมน์
 with col1:
-    st.image('./pic/263829-qgdtri-1.n.jpg')
+    st.write("Column 1 Content")
+    st.image('./pic/263829-qgdtri-1.n.jpg', width=200)
+
 with col2:
-    st.image('./pic/unnamed.jpg')
+    st.write("Column 2 Content")
+    st.image('./pic/unnamed.jpg', width=200)
